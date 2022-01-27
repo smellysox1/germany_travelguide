baa9feeb8f4563e55963ce6138bcee526f22005b
smellysox1 <80152017+smellysox1@users.noreply.github.com>
2022-01-27 13:54:38 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 
 function checkBlood(){
-if(){
-
+document.getElementById("details").innerHTML = "   <input type = "text" id = "details" placeholder = "mmmmm yes "+document.getElementById("details").value+"">"
 }
-}
+Webcam.set({
+    width : 350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+function takeSnapshot(){
+Webcam.snap(function(data_URI){
+document.getElementById("result").innerHTML = "<img id = 'captured_image' src = '"+data_URI+"'>";
+});}
