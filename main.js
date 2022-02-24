a434b557433f3f67c35dc242a4989803810977b1
smellysox1 <80152017+smellysox1@users.noreply.github.com>
2022-02-24 13:44:02 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,41 @@
 function checkBlood(){
 document.getElementById("details").innerHTML = "   <input type = "text" id = "details" placeholder = "mmmmm yes "+document.getElementById("details").value+"">"
 }
-Webcam.set({
-    width : 350,
-    height:300,
-    image_format:"png",
-    png_quality:90
-});
-camera = document.getElementById("camera");
-Webcam.attach("#camera");
-function takeSnapshot(){
-Webcam.snap(function(data_URI){
-document.getElementById("result").innerHTML = "<img id = 'captured_image' src = '"+data_URI+"'>";
-});}
+nosex=0;
+nosey=0;
+function preload(){
+clown_nose=loadImage("https://i.postimg.cc/bv945JxQ/moustache-png-17171.png");
+}
+function setup(){
+canvas = createCanvas(400,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(400,300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on("pose",gotPoses);
+}
+function draw(){
+image(video, 0,0, 400,300);
+fill(255,0,0);
+stroke(254,0,0);
+//circle(nosex,nosey,20);
+image(clown_nose,nosex,nosey,70,70);
+}
+function take_snapshot(){
+    save("funny_moustache.png");
+}
+function modelLoaded(){
+    console.log("poseNet initialised");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        nosex=results[0].pose.nose.x-30;
+        nosey=results[0].pose.nose.y-13;
+        console.log("nose x: "+nosex);
+        console.log("nose y: "+nosey);
+        
+    }
+}
