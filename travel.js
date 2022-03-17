4a0c3011e3ebe77554d0bd2596431e8566c42fd8
smellysox1 <80152017+smellysox1@users.noreply.github.com>
2022-03-17 13:39:39 +0000

    Update travel.js

--- travel.js
+++ travel.js
@@ -5,7 +5,7 @@ function preload(){
 clown_nose=loadImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSybDnOblfIPLD8CKj1sdRYT-VvOKKS6ITnVKHZuAhDE22UFxJK4CoyCylzut65s-VXTls&usqp=CAU");
 }
 function setup(){
-canvas = createCanvas(500,500);
+canvas = createCanvas(50,50);
 canvas.center();
 video = createCapture(VIDEO);
 video.size(500,500);
@@ -16,9 +16,8 @@ poseNet.on("pose",gotPoses);
 }
 function draw(){
 image(video, 0,0, 400,300);
-fill(255,0,0);
-stroke(254,0,0);
-//circle(nosex,nosey,20);
+
+
 image(clown_nose,nosex,nosey,800,800);
 }
 function take_snapshot(){
